f1489b5ed0419c23014cca26f78b9151f4c918e2
Ali Salehi <alisalehi69@live.com>
2022-02-06 00:30:01 +0300

    2 6 2022

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let burger = document.querySelector(".nav__toggle");
 burger.addEventListener("click", showMenu);
 function showMenu() {
-  // document.querySelector(".menu__list").classList.toggle("show");
+  document.querySelector(".menu__list").classList.toggle("show");
 }
