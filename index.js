966187d938a5262af8a2ed73033b6b08391b4919
Your Name <you@example.com>
2022-02-05 10:11:43 +0300

    itec  5 2 2022

---
+++ index.js
@@ -0,0 +1,5 @@
+let burger = document.querySelector(".nav__toggle");
+burger.addEventListener("click", showMenu);
+function showMenu() {
+  // document.querySelector(".menu__list").classList.toggle("show");
+}
